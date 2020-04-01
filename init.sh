05bc6dd7f3b1919ae774670e6f9fe0ef35f69aba
Ubuntu <ubuntu@nuc1.maas>
2020-04-01 14:24:18 +0000

    move pv to examples directory and use manager ns

--- init.sh
+++ init.sh
@@ -2,5 +2,3 @@ helm repo add stable https://kubernetes-charts.storage.googleapis.com
 helm repo add argo https://argoproj.github.io/argo-helm
 
 kubectl apply -f ./cluster-role-binding-manager.yaml
-
-./reset-manager.sh
