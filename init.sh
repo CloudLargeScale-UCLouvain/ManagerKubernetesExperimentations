3b8dcde10aae203de5aeae2442f085c76addc3ab
Ubuntu <ubuntu@livemigrations-agent-001-vm.cluster.local>
2020-07-02 09:38:12 +0000

    add init steps

--- init.sh
+++ init.sh
@@ -1,4 +1,13 @@
+sudo snap install kubectl --classic
+sudo snap install helm --classic
 helm repo add stable https://kubernetes-charts.storage.googleapis.com
 helm repo add argo https://argoproj.github.io/argo-helm
 
+cd chart
+helm dep update
+cd ..
+
+
 kubectl apply -f ./cluster-role-binding-manager.yaml
+kubectl apply -f ./cluster-role-binding-default.yaml
+kubectl apply -f ./secret-minio.yaml
