b0b355db6f7bdabd38e7341fc5b780f20b996ca9
Guillaume Rosinosky <guillaumerosinosky@yahoo.fr>
2021-01-24 21:21:04 +0000

    wip fix argo.minio not working (wip, not tested in argo) + revert to 4.10.1 mongo (need to test 8.2.3)

--- init.sh
+++ init.sh
@@ -2,6 +2,8 @@
 helm repo add stable https://charts.helm.sh/stable
 helm repo add argo https://argoproj.github.io/argo-helm
 helm repo add bitnami https://charts.bitnami.com/bitnami
+helm repo add minio https://helm.min.io/
+
 #helm install nginx-ingress stable/nginx-ingress --set controller.stats.enabled=true --set controller.kind=DaemonSet --set controller.daemonset.useHostPort=true  --set controller.service.type=NodePort --namespace kube-system --set defaultBackend.nodeSelector.tier=manager
 
 cd chart
